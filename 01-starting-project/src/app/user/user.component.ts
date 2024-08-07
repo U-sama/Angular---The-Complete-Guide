8a7facedf6b7314efaa75d8bfa94702747be0fe1
Osama Muhamed <63379624+U-sama@users.noreply.github.com>
2024-08-07 10:50:47 +0300

    content projection

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -1,9 +1,10 @@
 import { Component, computed, Input, signal, input, Output, EventEmitter, output } from '@angular/core';
+import { CardComponent } from "../shared/card/card.component";
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
